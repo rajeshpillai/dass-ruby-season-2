c0cc4f6b72385821061f26a2dc8c1fbffb3efaf1
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-07-29 15:31:44 +0530

    Cleanup

--- 11-block-more.rb
+++ 11-block-more.rb
@@ -1,15 +1,12 @@
 # Passing proc and block together
 
+
 def abc(x, y, &b)
-  p &b
   puts x 
   y.call 
   b.call if block_given?
 end
 
-def receive_block(b) 
-  p b
-end
 
 abc(5, proc { puts "I am a proc"}) do 
   puts "I am the block"

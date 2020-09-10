ea59b5b254877153c484c6e25d01a45c199a2476
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-10 07:36:55 +0530

    First failure for should matcher

--- tdd/qtest.rb
+++ tdd/qtest.rb
@@ -15,3 +15,28 @@ end
 def is_equal(expected, actual)
   expected == actual   # true or false
 end
+
+# Open class
+class Object
+  def should 
+    ComparisonAssertion.new(self)
+  end
+end
+
+class ComparisonAssertion
+  def initialize (actual)
+    @actual = actual
+  end
+
+  def ==(expected)
+    unless @actual == expected
+      raise AssertionError.new(
+        "Expected #{expected.inspect} but got #{@actual.inspect}"
+      )
+    end
+  end
+end
+
+class AssertionError < RuntimeError
+
+end
--- tdd/simple_test.rb
+++ tdd/simple_test.rb
@@ -1,5 +1,18 @@
 require_relative "qtest"
 
+
+describe "some examples" do 
+  it "can pass" do 
+    # actual == expected
+    (1 + 1).should == 2
+  end
+
+  it "can pass after failing" do 
+    (1 + 1).should == 3
+  end
+end
+
+
 class SimpleTest
   #macro methods ruby
   describe "Basic test"  do 

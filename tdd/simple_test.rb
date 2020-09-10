5506381a4fbc1dc63ca9508e913071d6c9af9f5d
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-10 07:38:42 +0530

    Note, after failing test, rest of the test are not running

--- tdd/simple_test.rb
+++ tdd/simple_test.rb
@@ -3,13 +3,18 @@ require_relative "qtest"
 
 describe "some examples" do 
   it "can pass" do 
+    puts "1"
     # actual == expected
     (1 + 1).should == 2
   end
 
-  it "can pass after failing" do 
-    puts "ok"
+  it "can fail" do
+    puts "2"
     (1 + 1).should == 3
+  end
+
+  it "can pass after failing" do 
+    puts "3"
   end
 end
 

be67f78e846bb35a3d9387014440ae8639f77945
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-09-10 07:40:53 +0530

    Note, after failing test, rest of the test are not running

--- tdd/qtest.rb
+++ tdd/qtest.rb
@@ -3,14 +3,25 @@ def describe (desc, &block)
   block.call
 end
 
+
 def it (desc, &block) 
-  result = block.call 
-  if result 
-    puts "     #{desc.ljust(40, '.')} passed"
-  else
-    puts "     #{desc.ljust(40, '.')} failed"
+  begin 
+    result = block.call 
+  rescue Exception => e 
+    puts e 
+    puts e.backtrace
   end
 end
+
+
+# def it (desc, &block) 
+#   result = block.call 
+#   if result 
+#     puts "     #{desc.ljust(40, '.')} passed"
+#   else
+#     puts "     #{desc.ljust(40, '.')} failed"
+#   end
+# end
 
 def is_equal(expected, actual)
   expected == actual   # true or false
